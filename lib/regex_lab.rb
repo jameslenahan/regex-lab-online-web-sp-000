ffaabaa60ffb1625fa51d05fe6d0abb5975bc7c8
James Lenahan <james.k.lenahan@gmail.com>
2019-11-22 18:41:22 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,10 @@
 def starts_with_a_vowel?(word)
-  word.match(/\aeiou/)
+  if word.match(/^aeiou/)
+    true
+  else
+    false
+  end
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
