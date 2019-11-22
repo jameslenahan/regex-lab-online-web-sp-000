fb3ed0e205375b704c1e4b8ad92d4d94822142ea
James Lenahan <james.k.lenahan@gmail.com>
2019-11-22 18:49:39 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,10 +17,13 @@ def words_five_letters_long(text)
 
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/ABCDEFGHIJKLMNOPQRSTUVWXYZ\w+\!?.,/)
+def first_word_capitalized_and_ends_with_punctuation_2?(text)
+  if text.match(/^[A-Z]/) && text.match(/[\.]$/)
+    true
+  else 
+    false
+  end
 end
-
 def valid_phone_number?(phone)
 
 end
